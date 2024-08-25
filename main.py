b6f22fc8898b29f057fb4d126ce9cad5ffd56234
wyattnorris <wyattfnorris@gmail.com>
2024-08-25 13:48:50 -0400

    Changed config variable

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime
 
 
-CONFIG_FILE = 'config-worldleague.json' #see config-example.json for more information
+CONFIG_FILE = 'config.json' #see config-example.json for more information
 
 class SleeperSheets:
     """
